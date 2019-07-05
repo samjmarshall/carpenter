12865ca268f724388b11b5aa0203d9de992a5592
Sam Marshall <sam@foundu.com.au>
2019-07-05 15:01:02 +1000

    Removed comments and install function

--- cmd/imageBuild.go
+++ cmd/imageBuild.go
@@ -34,14 +34,4 @@ var imageBuildCmd = &cobra.Command{
 
 func init() {
 	imageCmd.AddCommand(imageBuildCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// imageBuildCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// imageBuildCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/imageTest.go
+++ cmd/imageTest.go
@@ -33,14 +33,4 @@ var imageTestCmd = &cobra.Command{
 
 func init() {
 	imageCmd.AddCommand(imageTestCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// imageTestCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// imageTestCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
